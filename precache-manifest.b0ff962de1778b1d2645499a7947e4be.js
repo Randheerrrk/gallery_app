a0a72d257782e069b5d36b42da613ea2376540f7
Randheerrrk <Randheerrrk@gmail.com>
2020-07-20 06:41:21 +0530

    Updates

---
+++ precache-manifest.b0ff962de1778b1d2645499a7947e4be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eed8a5c4213288ddc9a8eee9c1c8392d",
+    "url": "/Randheerrrk/gallery_app/index.html"
+  },
+  {
+    "revision": "056af94da13dd9c842fc",
+    "url": "/Randheerrrk/gallery_app/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "520abe580234c61cc061",
+    "url": "/Randheerrrk/gallery_app/static/css/main.8abe8b4d.chunk.css"
+  },
+  {
+    "revision": "056af94da13dd9c842fc",
+    "url": "/Randheerrrk/gallery_app/static/js/2.e77d7be0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Randheerrrk/gallery_app/static/js/2.e77d7be0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "520abe580234c61cc061",
+    "url": "/Randheerrrk/gallery_app/static/js/main.d746da1f.chunk.js"
+  },
+  {
+    "revision": "7d013b7973bf4e264593",
+    "url": "/Randheerrrk/gallery_app/static/js/runtime-main.c13fbe2f.js"
+  }
+]);
